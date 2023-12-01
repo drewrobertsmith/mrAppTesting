e50aa4bc2e46f0ad956964ed61de138156fe55c8
drewrobertsmith <drew.robert.smith@gmail.com>
2023-12-01 16:02:43 -0600

    Added Async Storage to cache the programs request with a 60min expiration

--- src/services/programsAndClipsRequest.service.js
+++ src/services/programsAndClipsRequest.service.js
@@ -1,15 +1,53 @@
-const BASE_URL = 'https://api.omny.fm';
-const ORG_ID = 'a8cdbf10-d816-4c77-9e79-aa1c012547e1';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+//api deats
+const BASE_URL = "https://api.omny.fm";
+const ORG_ID = "a8cdbf10-d816-4c77-9e79-aa1c012547e1";
+
+//cache control
+const CACHE_KEY = "ProgramsCache";
+const CACHE_EXPIRATION_MINUTES = 60; // Cache Expiration Time
 
 export async function ProgramsRequest({ setIsLoading, setPrograms }) {
   setIsLoading(true);
   try {
+    // Check cache
+    const cachedData = await AsyncStorage.getItem(CACHE_KEY);
+    if (cachedData) {
+      const { data, timestamp } = JSON.parse(cachedData);
+      const isCacheValid =
+        (new Date().getTime() - timestamp) / 60000 < CACHE_EXPIRATION_MINUTES;
+
+      //console.log("Cached data found", { cachedData: data, isCacheValid });
+
+      if (isCacheValid) {
+        setPrograms(data);
+        setIsLoading(false);
+        return;
+      } else {
+        console.log("Cache expired, fetching new data");
+      }
+    } else {
+      console.log("No cached data found, fetching from API");
+    }
+
+    // API Request
     const response = await fetch(`${BASE_URL}/orgs/${ORG_ID}/programs`);
-    console.log(response);
     const result = await response.json();
-    setPrograms(result.Programs.filter((n) => n.Network === 'Moody Radio'));
+    const filteredPrograms = result.Programs.filter(
+      (n) => n.Network === "Moody Radio"
+    );
+    setPrograms(filteredPrograms);
+
+    // Cache data
+    const cacheValue = {
+      data: filteredPrograms,
+      timestamp: new Date().getTime(),
+    };
+    await AsyncStorage.setItem(CACHE_KEY, JSON.stringify(cacheValue));
+    console.log("New data cached");
   } catch (error) {
-    console.log(error);
+    console.log("Error fetching data:", error);
   } finally {
     setIsLoading(false);
   }
@@ -18,7 +56,9 @@ export async function ProgramsRequest({ setIsLoading, setPrograms }) {
 export async function ClipsRequest({ setIsLoading, setEpisodes, show }) {
   setIsLoading(true);
   try {
-    const response = await fetch(`${BASE_URL}/orgs/${ORG_ID}/programs/${show.Id}/clips`);
+    const response = await fetch(
+      `${BASE_URL}/orgs/${ORG_ID}/programs/${show.Id}/clips`
+    );
     const result = await response.json();
     setEpisodes(result.Clips);
   } catch (error) {
